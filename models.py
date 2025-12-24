b7fdc996aa571aec2a683043feff3868646751f7
MKING301 <33675411+MKing301@users.noreply.github.com>
2025-12-24 10:15:32 -0500

    Update models

---
+++ models.py
@@ -0,0 +1,13 @@
+# models.py
+from sqlalchemy import Boolean, Column, Integer, String
+
+from database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password_hash = Column(String, nullable=False)
+    is_admin = Column(Boolean, default=False)
